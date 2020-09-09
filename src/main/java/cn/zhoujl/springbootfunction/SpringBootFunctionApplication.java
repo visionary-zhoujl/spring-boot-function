b4bb30d530aeb92cfd67090e61aafaa6ee46e5c2
ZjlAndLxx <840191760@qq.com>
2020-09-09 22:14:20 +0800

    feat(spring-boot-function):建项目，用于实现一些场景场景

---
+++ src/main/java/cn/zhoujl/springbootfunction/SpringBootFunctionApplication.java
@@ -0,0 +1,13 @@
+package cn.zhoujl.springbootfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFunctionApplication.class, args);
+    }
+
+}
